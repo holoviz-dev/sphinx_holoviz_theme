b294d4c5f9bece48e3d5bd3fb3aabb89bd3ec01b
Julia Signell <jsignell@gmail.com>
2019-05-10 14:54:45 -0400

    Fixing up indentation

--- sphinx_pyviz_theme/__init__.py
+++ sphinx_pyviz_theme/__init__.py
@@ -1,8 +1,7 @@
 import os
-
- from ._version import get_versions
+from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
- def setup(app):
-    app.add_html_theme('sphinx_pyviz_theme', os.path.abspath(path.dirname(__file__)))
+def setup(app):
+   app.add_html_theme('sphinx_pyviz_theme', os.path.abspath(path.dirname(__file__)))
